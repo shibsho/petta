bfd661fa2f26cd81e14a3706bc6d6b75d24e10ca
shibsho <shibsho@gmail.com>
2017-02-05 09:18:03 +0900

    不要なjavascriptファイルを削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,18 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-  $(function(){
-		$(function() {
-        var $header = $('#top-head');
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 50) {
-                $header.addClass('fixed');
-            } else {
-                $header.removeClass('fixed');
-            }
-        });
-    });
-});
-  
-
