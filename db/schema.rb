dd805de43c1fe725204907c844cc451a8e4f7bf8
shibsho <shibsho@gmail.com>
2017-02-14 00:23:13 +0900

    バグを解消

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170209125837) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "note_id"
@@ -29,10 +23,9 @@ ActiveRecord::Schema.define(version: 20170209125837) do
     t.string   "image"
     t.string   "category"
     t.string   "content"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "category_name"
   end
 
   create_table "users", force: :cascade do |t|
